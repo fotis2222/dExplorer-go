97801a1744f7a5df23fd60aded0014976e9c1eb4
fotis2222 <nitsas2012@gmail.com>
2024-04-07 10:56:01 +0300

    added attack but you cannot lose hp

--- main.go
+++ main.go
@@ -14,28 +14,44 @@ func (p Player) Attack(enemyHP int) {
 }
 
 func main() {
-    player := Player {
-        hp: 100,
-    }
+	player := Player{
+		hp: 100,
+	}
 
+	fmt.Println("Welcome to the dungeon of Supremium!")
+	fmt.Println("Press Enter to begin!")
+	fmt.Scanln()
+	var see int
+	var option string
+	for {
+		see = rand.Intn(2)
+		if player.hp <= 0 {
+			break
+		} else {
+			if see == 0 {
+				fmt.Println("You see nothing, press Enter to continue")
+				fmt.Scanln()
+			} else {
+				fmt.Println("You see a spider\nOptions:")
+				fmt.Println("(1) Attack")
+				fmt.Println("(2) Dodge")
+				fmt.Print("Option >> ")
+				fmt.Scanln(&option)
+				if option == "1" {
+					player.Attack(5)
+					fmt.Printf("You now have %d hp\n", player.hp)
+					fmt.Println("Press enter to continue")
+					fmt.Scanln()
+				} else if option == "2" {
+					fmt.Println("You decided to dodge, press Enter to continue")
+					fmt.Scanln()
+				} else {
+					fmt.Println("Sorry, but your option was invalid. (No hp was deducted)")
+					fmt.Println("Press enter to continue")
+					fmt.Scanln()
+				}
+			}
+		}
 
-    fmt.Println("Welcome to the dungeon of Supremium!")
-    fmt.Println("Press Enter to begin!")
-    fmt.Scanln()
-    var see int
-
-    for {
-        see = rand.Intn(2)
-        if player.hp <= 0 {
-            break
-        } else {
-            if see == 0 {
-                fmt.Println("You see nothing, press Enter to continue")
-                fmt.Scanln()
-            } else {
-                // TODO: insert code
-            }
-        }
-
-    }
+	}
 }
