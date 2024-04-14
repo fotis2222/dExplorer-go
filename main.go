b6386cda9d6308889d1945914ef7e18d4505d79e
fotis2222 <nitsas2012@gmail.com>
2024-04-14 17:13:20 +0300

    added super snake and super spider

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	var see int
 	var option string
 	for {
-		see = rand.Intn(10)
+		see = rand.Intn(12)
 		if player.hp <= 0 {
 			fmt.Println("You have lost the game, press Enter to exit")
 			fmt.Scanln()
@@ -192,6 +192,48 @@ func main() {
 				fmt.Printf("You now have %d xp\n", player.xp)
 				fmt.Println("Press enter to continue")
 				fmt.Scanln()
+			} else if see == 10 {
+				fmt.Println("You see a super spider!\nOptions:")
+				fmt.Println("(1) Attack")
+				fmt.Println("(2) Dodge")
+				fmt.Print("Option >> ")
+				fmt.Scanln(&option)
+				if option == "1" {
+					player.Attack(30)
+					player.xp += 20
+					fmt.Printf("You now have %d hp\n", player.hp)
+					fmt.Printf("You now have %d xp\n", player.xp)
+					fmt.Println("Press enter to continue")
+					fmt.Scanln()
+				} else if option == "2" {
+					fmt.Println("You decided to dodge, press Enter to continue")
+					fmt.Scanln()
+				} else {
+					fmt.Println("Sorry, but your option was invalid. (No hp was deducted)")
+					fmt.Println("Press enter to continue")
+					fmt.Scanln()
+				}
+			} else if see == 11 {
+				fmt.Println("You see a super snake!\nOptions:")
+				fmt.Println("(1) Attack")
+				fmt.Println("(2) Dodge")
+				fmt.Print("Option >> ")
+				fmt.Scanln(&option)
+				if option == "1" {
+					player.Attack(50)
+					player.xp += 35
+					fmt.Printf("You now have %d hp\n", player.hp)
+					fmt.Printf("You now have %d xp\n", player.xp)
+					fmt.Println("Press enter to continue")
+					fmt.Scanln()
+				} else if option == "2" {
+					fmt.Println("You decided to dodge, press Enter to continue")
+					fmt.Scanln()
+				} else {
+					fmt.Println("Sorry, but your option was invalid. (No hp was deducted)")
+					fmt.Println("Press enter to continue")
+					fmt.Scanln()
+				}
 			}
 		}
 	}
